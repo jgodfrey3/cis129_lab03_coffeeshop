5d9d77d40bd356047da9927e5b481d28b159c17b
jgodfrey3 <158596982+jgodfrey3@users.noreply.github.com>
2024-02-14 09:52:10 -0700

    adds cis129_lab03_coffeshop.py

---
+++ cis129_lab03_coffeeshop.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""This program is a virtual coffee shop. You can 'buy' coffee and muffins, and it will calculate the price based on the number purchased,
+calculate the sales tax, and print you a receipt."""
+
+#prints the top border and introductory text
+print('***************************************\nMy Coffee and Muffin Shop')
+
+#asks the user for their orders and stores them as an integer
+coffeeNumber = int(input('Number of coffees bought?'))
+
+muffinNumber = int(input('Number of muffins bought?'))
+
+#prints the closing border
+print('***************************************\n')
+
+#multiplies number of orders by price to get cost of specific items
+coffeeCost = 5 * coffeeNumber
+
+muffinCost = 8 * muffinNumber
+
+#adds together cost of items to get total cost
+sumCost = coffeeCost + muffinCost
+
+#calculates the final price after sales tax
+finalCost = sumCost * 1.06
+
+#calculates the sales tax
+salesTax = sumCost * 0.06
+
+#prints the opening border and first line of user's receipt
+print('***************************************\nMy Coffee and Muffin Shop Receipt')
+
+#prints the dollar amount spent on coffee
+print(str(coffeeNumber),
+    'Coffee(s) at 5$ Each: $',
+    (str(coffeeCost)) + ('.00'))
+
+#prints the dollar amount spent on muffins
+
+print(str(muffinNumber),
+    'Muffin(s) at 8$ Each: $',
+    (str(muffinCost)) + ('.00'))
+
+#prints the sales tax
+print('6% tax: $' + (str(salesTax)))
+
+#prints seperator line
+print('---------')
+
+#prints the final cost
+print('Total: $' + (str(finalCost)))
+
+#prints the closing border of the user's receipt
+print('***************************************')
